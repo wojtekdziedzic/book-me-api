f7e0e933a0a35d9008bc58d3587d531c0168ffc4
WojtekD <wpd.web@gmail.com>
2019-07-09 21:48:59 +0200

    add db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OffersModule } from './offers/offers.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [OffersModule],
+  imports: [OffersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'book-me',
+      password: 'book-me',
+      database: 'book-me',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
