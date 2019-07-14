8746cc1c1609a50f7078903113ce2bdd1eeb4aca
WojtekD <wpd.web@gmail.com>
2019-07-14 12:36:46 +0200

    update test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,7 +15,7 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
+    it('should return "I\'m working :)"', () => {
       expect(appController.index()).toBe('I\'m working :)');
     });
   });
