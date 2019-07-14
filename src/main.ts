f2b4a3b799b8bb9c367afd54e7da034bce1cde97
WojtekD <wpd.web@gmail.com>
2019-07-14 09:24:29 +0200

    add cors to app

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: console,
   });
+
+  await app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept',
+  });
+
   await app.listen(3000);
 }
 
